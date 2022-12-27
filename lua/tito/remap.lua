5963ffefa814a92d8219d9fed9b4fd4b752f41f6
Macbook <macbook@MacBook-de-Macbook.local>
2022-12-27 19:34:44 -0300

    chore: add some extra remaps

--- lua/tito/remap.lua
+++ lua/tito/remap.lua
@@ -4,6 +4,16 @@ vim.keymap.set("c", "W", vim.cmd.write)
 vim.keymap.set("n", "<C-h>", vim.cmd.bprev)
 vim.keymap.set("n", "<C-l>", vim.cmd.bnext)
 vim.keymap.set("n", "<leader>db", vim.cmd.bd)
+
+-- go to previous opened file current in # register
 vim.keymap.set("n", "<leader>l", function () vim.cmd.edit("#") end)
 
+-- move selected lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
+-- when paste on a highlighted line, keep the copied value and throw the selected text to void buffer
+vim.keymap.set("x", "<leader>p", "\"_dp")
+
+-- select current word and change in file every match based on the text entered
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
